c11e39ba0958a93dd596a411f8f2d6db184c3d59
luckykun88 <kmisigaro@outlook.com>
2021-10-27 16:32:24 +0300

    staging the app to heroku

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -10,24 +10,45 @@ class SMSController extends Controller
 {
     public function sendSMS(){
 
-        $response = Http::withHeaders([
-            'Basic Auth' =>  base64_encode("24c3943f93fb04b2:NzRiYWRiZWM4NGE3ZTY4MjMxZDZkZjA2ZjRiY2JmNWE2NzE0ZmUzYTM5MmFmZDFkZGEyNzE4N2YyODJlMjM0ZQ=="),
-            'Content-Type' => 'application/json'
-        ])->post('https://apisms.beem.africa/v1/send', [
-            'source_addr' => '15320',
-            'encoding'=>0,
+        $api_key= env('BEEM_API_KEY');
+        $secret_key = env('BEEM_SECRET_KEY');
+
+        $postData = array(
+            'source_addr' => env('BEEM_SOURCE_ADDRESS'),
+            'encoding'=> 0,
             'schedule_time' => '',
             'message' => 'Hello World',
             'recipients' => [
                 array('recipient_id' => '1','dest_addr'=>'255782835136')
-            ]
-        ]);
+                ]
+        );
 
-        if($response->successful()){
-            return response()->json('Success', 200);
+        $Url ='https://apisms.beem.africa/v1/send';
+
+        $ch = curl_init($Url);
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt_array($ch, array(
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization:Basic ' . base64_encode("$api_key:$secret_key"),
+                'Content-Type: application/json'
+            ),
+            CURLOPT_POSTFIELDS => json_encode($postData)
+        ));
+
+        $response = curl_exec($ch);
+
+        if($response === FALSE){
+                echo $response;
+
+            die(curl_error($ch));
         }
+        var_dump($response);
 
-        return \response()->json('Failed', 400);
     }
 
 
