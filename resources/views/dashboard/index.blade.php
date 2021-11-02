51017199ac5d64bd8726d94ce02142b888b7003c
luckykun88 <kmisigaro@outlook.com>
2021-11-02 16:15:34 +0300

    landing page

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,7 +14,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Cards
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Messages
                             </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                0
@@ -28,6 +28,43 @@
             </div>
         </div>
 
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Beneficiaries
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                               0
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="far fa-address-card fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Age range
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                               0
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="far fa-address-card fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     
 
